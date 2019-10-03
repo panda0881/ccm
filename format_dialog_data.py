f3fe0336f7062a900d8414bf592ac1e5ebf0e376
hongmingzhang <hzhangal0330@gmail.com>
2019-10-03 22:03:58 +0800

    prepare data

--- format_dialog_data.py
+++ format_dialog_data.py
@@ -8,8 +8,8 @@ with open('dialog_dataset/dialogues_train.txt', 'r', encoding='utf-8') as f:
         sentence_number = len(tmp_sentences)
         for i in range(len(tmp_sentences)-1):
             tmp_pair = dict()
-            tmp_pair['post'] = tmp_sentences[i]
-            tmp_pair['response'] = tmp_sentences[i+1]
+            tmp_pair['post'] = tmp_sentences[i].strip()
+            tmp_pair['response'] = tmp_sentences[i+1].strip()
             dialog_train.append(tmp_pair)
 
 with open('dialog_dataset/formatted_train.json', 'w') as f:
@@ -22,8 +22,8 @@ with open('dialog_dataset/dialogues_validation.txt', 'r', encoding='utf-8') as f
         sentence_number = len(tmp_sentences)
         for i in range(len(tmp_sentences)-1):
             tmp_pair = dict()
-            tmp_pair['post'] = tmp_sentences[i]
-            tmp_pair['response'] = tmp_sentences[i+1]
+            tmp_pair['post'] = tmp_sentences[i].strip()
+            tmp_pair['response'] = tmp_sentences[i+1].strip()
             dialog_dev.append(tmp_pair)
 
 with open('dialog_dataset/formatted_dev.json', 'w') as f:
@@ -32,12 +32,12 @@ with open('dialog_dataset/formatted_dev.json', 'w') as f:
 dialog_test = list()
 with open('dialog_dataset/dialogues_test.txt', 'r', encoding='utf-8') as f:
     for line in f:
-        tmp_sentences = line.split('__eou__')[:-1]
+        tmp_sentences = line.split(' __eou__ ')[:-1]
         sentence_number = len(tmp_sentences)
         for i in range(len(tmp_sentences)-1):
             tmp_pair = dict()
-            tmp_pair['post'] = tmp_sentences[i]
-            tmp_pair['response'] = tmp_sentences[i+1]
+            tmp_pair['post'] = tmp_sentences[i].strip()
+            tmp_pair['response'] = tmp_sentences[i+1].strip()
             dialog_test.append(tmp_pair)
 
 with open('dialog_dataset/formatted_test.json', 'w') as f:
