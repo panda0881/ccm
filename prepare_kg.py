0ad397c0cb828073df32547293abfad41451bbb3
hongmingzhang <hzhangal0330@gmail.com>
2019-10-04 16:49:37 +0800

    prepare data

--- prepare_kg.py
+++ prepare_kg.py
@@ -1,20 +1,20 @@
 import ujson as json
-
+from tqdm import tqdm
 
 with open('kgs/lemmatized_commonsense_knowledge.json', 'r', encoding='utf-8') as f:
     test_data = json.load(f)
 
 with open('kgs/conceptnet.txt', 'w', encoding='utf-8') as f:
     for r in test_data:
-        for tmp_k in test_data[r]:
-            print(tmp_k)
+        for tmp_k in tqdm(test_data[r]):
+            # print(tmp_k)
             f.write(r)
             f.write('\t')
             f.write(tmp_k['head'])
             f.write('\t')
             f.write(tmp_k['tail'])
             f.write('\n')
-            break
+            # break
 
 
 print('end')
