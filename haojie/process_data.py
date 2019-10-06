b496137c95d30c3308b5ba4811378c937fc58a8a
hongmingzhang <hzhangal0330@gmail.com>
2019-10-06 14:09:18 +0800

    try haojie's model

--- haojie/process_data.py
+++ haojie/process_data.py
@@ -144,9 +144,9 @@ def process_data(kg_path, output_path):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-input", type=str, default='/home/guest/hzhangal/ccm/kgs/conceptnet.txt',
+parser.add_argument("--input", type=str, default='/home/guest/hzhangal/ccm/kgs/conceptnet.txt',
                         help="choose which gpu to use")
-parser.add_argument("-output", type=str, default='/home/guest/hzhangal/ccm/haojie/data/conceptnet/',
+parser.add_argument("--output", type=str, default='/home/guest/hzhangal/ccm/haojie/data/conceptnet/',
                         help="choose which gpu to use")
 args = parser.parse_args()
 
