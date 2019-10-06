754af68d31770e4c02fdee9abb2fd4a8156dd92f
hongmingzhang <hzhangal0330@gmail.com>
2019-10-06 14:27:51 +0800

    try haojie's model

---
+++ haojie/tmp.bash
@@ -0,0 +1,4 @@
+python process_data.py --input /home/guest/hzhangal/ccm/kgs/COMET_original_1.txt --output /home/guest/hzhangal/ccm/haojie/data/COMET_original_1/
+python process_data.py --input /home/guest/hzhangal/ccm/kgs/COMET_external_10.txt --output /home/guest/hzhangal/ccm/haojie/data/COMET_external_10/
+python process_data.py --input /home/guest/hzhangal/ccm/kgs/LAMA_original_1.txt --output /home/guest/hzhangal/ccm/haojie/data/LAMA_original_1/
+python process_data.py --input /home/guest/hzhangal/ccm/kgs/LAMA_external_10.txt --output /home/guest/hzhangal/ccm/haojie/data/LAMA_external_10/
